2981020638b70ed6d22d3e7fc611bba7ff93170c
Verdi <152645717+tfVerdi@users.noreply.github.com>
2024-11-03 16:46:21 -0300

    Prevented file rewriting prompt and improved readability

--- footage_reencoder.py
+++ footage_reencoder.py
@@ -5,54 +5,65 @@ import os
 with open("data.json") as f:
     data = json.load(f)
 
-ffmpeg_path = data['global'][0]['ffmpeg_path']
-handbrakeCLI_path = data['global'][0]['handbrakeCLI_path']
-inputFile_path = data['global'][0]['input_file_path']
-outputFile_path = data['global'][0]['output_file_path']
+ffmpeg_path = data['global']['ffmpeg_path']
+handbrakeCLI_path = data['global']['handbrakeCLI_path']
+input_file_path = data['global']['input_file_path']
+input_file_path = data['global']['output_file_path']
 
-os.chdir(inputFile_path)
-
-directory_entries = os.listdir()
-directory_entries = directory_entries[1:]
-for i in range(len(directory_entries)):
-    directory_entries[i] = os.getcwd() + "\\" + directory_entries[i]
+os.chdir(input_file_path)
+directory_entries = os.listdir()[1:]
     
-def jointracks(video_input_path: str, outputname: str, ffmpeg_path: str = ffmpeg_path):
+def JoinTracks(video_input_path: str, output_name: str, ffmpeg_path: str = ffmpeg_path):
     command = [
             ffmpeg_path,
-            "-i", video_input_path + "\\take0000\\video.mp4", "-i", video_input_path + "\\take0000\\audio.wav", 
-            video_input_path + "\\take0000\\" + outputname + ".mp4"
+            "-i", video_input_path + "\\take0000\\video.mp4",
+            "-i", video_input_path + "\\take0000\\audio.wav", 
+            video_input_path + "\\take0000\\" + output_name + ".mp4"
             ]
     subprocess.run(args=command)
 
-def recode(video_input_path: str, outputname: str, handbrakeCLI_path: str = handbrakeCLI_path, outputFile_path: str = outputFile_path):
+def Recode(video_input_path: str, output_name: str, handbrakeCLI_path: str = handbrakeCLI_path, input_file_path: str = input_file_path):
     command = [
             handbrakeCLI_path,
-            "-i", video_input_path + "\\take0000\\" + outputname + ".mp4",
-            "-o", outputFile_path + outputname + ".mp4",
-            "-e", "x264", # Encoder
+            "-i", video_input_path + "\\take0000\\" + output_name + ".mp4",
+            "-o", input_file_path + output_name + ".mp4",
+            "-e", "x264",   # Encoder
             "-b", "130000", # Bitrate [kbps]
-            "-r", "60",   # FPS for output  
-            "--cfr",      # Constant framerate
+            "-r", "60",     # FPS for output  
+            "--cfr",        # Constant framerate
             "--width", "1920",  
             "--height", "1080",
             "--no-deinterlace"
             ]
     subprocess.run(args=command)
 
+def PrintRemaining(videos_remaining: int) -> None:
+    print(f"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print(f"@                                              @")
+    print(f"@                                              @")
+    if (videos_remaining // 10) == 0:
+        print(f"@         Video done! {videos_remaining} remaining :)           @")
+    elif (videos_remaining // 100) > 0:
+        print(f"@       Video done! {videos_remaining} remaining :)           @")
+    elif (videos_remaining // 10) > 0:
+        print(f"@       Video done! {videos_remaining} remaining :)            @")
+    print(f"@                                              @")
+    print(f"@                                              @")
+    print(f"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    
+
+
 if __name__ == "__main__":
-    counter = 0
+    videos_remaining = len(directory_entries)
     for video in directory_entries:
-        outputname = video.split("\\")[-1]
-        jointracks(video, outputname)
-        recode(video, outputname)
-        os.remove(video + "\\take0000\\" + outputname + ".mp4")
-        counter += 1
-        print(f"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-        print(f"@                                              @")
-        print(f"@                                              @")
-        print(f"@         Video done! {len(directory_entries) - counter} remaining :)          @")
-        print(f"@                                              @")
-        print(f"@                                              @")
-        print(f"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        output_name = video
+        new_path = os.getcwd() + "\\" + video
+        if not os.path.isfile(new_path):
+            JoinTracks(video, output_name)
+            Recode(video, output_name)
+            os.remove(video + "\\take0000\\" + output_name)
+        videos_remaining -= 1
+        videos_remaining[video] = new_path
+
+    PrintRemaining(videos_remaining)
     
--- single_footage_reencoder.py
+++ single_footage_reencoder.py
@@ -2,7 +2,7 @@
 ######################################## SETTINGS #########################################
 
 path_to_render = "D:\\TF2b\\melies_output\\rec2024-05-16_16-36-34_405913-407747_bm"
-take_to_render = "0001"
+take_to_render = "0000"
 
 ###########################################################################################
 
@@ -13,27 +13,28 @@ import os
 with open("data.json") as f:
     data = json.load(f)
 
-ffmpeg_path = data['global'][0]['ffmpeg_path']
-handbrakeCLI_path = data['global'][0]['handbrakeCLI_path']
-outputFile_path = data['global'][0]['output_file_path']
+ffmpeg_path = data['global']['ffmpeg_path']
+handbrakeCLI_path = data['global']['handbrakeCLI_path']
+output_file_path = data['global']['output_file_path']
 
-outputname = path_to_render.split("\\")[-1]
+output_name = path_to_render.split("\\")[-1]
 
 os.chdir("D:\\TF2b\\melies_output")
     
-def jointracks(video_input_path: str, outputname: str = outputname, take_to_render: str = take_to_render, ffmpeg_path: str = ffmpeg_path):
+def JoinTracks(video_input_path: str, output_name: str = output_name, take_to_render: str = take_to_render, ffmpeg_path: str = ffmpeg_path):
     command = [
             ffmpeg_path,
-            "-i", video_input_path + "\\take" + take_to_render + "\\video.mp4", "-i", video_input_path + "\\take" + take_to_render + "\\audio.wav", 
-            video_input_path + "\\take" + take_to_render + "\\" + outputname + "_single.mp4"
+            "-i", video_input_path + "\\take" + take_to_render + "\\video.mp4",
+            "-i", video_input_path + "\\take" + take_to_render + "\\audio.wav", 
+            video_input_path + "\\take" + take_to_render + "\\" + output_name + "_single.mp4"
             ]
     subprocess.run(args=command)
 
-def recode(video_input_path: str, outputname: str = outputname, take_to_render: str = take_to_render, handbrakeCLI_path: str = handbrakeCLI_path, outputFile_path: str = outputFile_path):
+def Recode(video_input_path: str, output_name: str = output_name, take_to_render: str = take_to_render, handbrakeCLI_path: str = handbrakeCLI_path, output_file_path: str = output_file_path):
     command = [
             handbrakeCLI_path,
-            "-i", video_input_path + "\\take" + take_to_render + "\\" + outputname + "_single.mp4",
-            "-o", outputFile_path + outputname + "_single.mp4",
+            "-i", video_input_path + "\\take" + take_to_render + "\\" + output_name + "_single.mp4",
+            "-o", output_file_path + output_name + "_single.mp4",
             "-e", "x264", # Encoder
             "-b", "30000", # Bitrate [kbps]
             "-r", "60",   # FPS for output  
@@ -43,10 +44,10 @@ def recode(video_input_path: str, outputname: str = outputname, take_to_render: 
             "--no-deinterlace"
             ]
     subprocess.run(args=command)
-    
-jointracks(path_to_render)
-recode(path_to_render)
-os.remove(path_to_render + "\\take" + take_to_render + "\\" + outputname + "_single.mp4")
-print("\n! Deleted mid-process video\n\n")
+if __name__ == "__main__":
+    JoinTracks(path_to_render)
+    Recode(path_to_render)
+    os.remove(path_to_render + "\\take" + take_to_render + "\\" + output_name + "_single.mp4")
+    print("\n! Deleted mid-process video\n\n")
 
 
